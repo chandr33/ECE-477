eac78b1f1779b018f235205305e88eb903071222
Qifan Chang <qchang@Qifans-MacBook.local>
2018-10-28 13:36:38 -0400

    deleted all firmware files

--- nrf52SDK/examples/ble_peripheral/ble_app_hids_keyboard/pca10040/s132/ses/Output/ble_app_hids_keyboard_pca10040_s132 Release/Obj/ses_startup_nrf52.d
+++
@@ -1,2 +0,0 @@
-Output/ble_app_hids_keyboard_pca10040_s132\ Release/Obj/ses_startup_nrf52.o: \
- /Users/qchang/Library/Mobile\ Documents/com~apple~CloudDocs/Dev/ECE-477/Segger\ Internal\ Files/ses_startup_nrf52.s
--- nrf52SDK/examples/ble_peripheral/ble_app_hids_keyboard/pca10040/s132/ses/Output/ble_app_hids_keyboard_pca10040_s132 Release/Obj/ses_startup_nrf_common.d
+++
@@ -1,2 +0,0 @@
-Output/ble_app_hids_keyboard_pca10040_s132\ Release/Obj/ses_startup_nrf_common.o: \
- /Users/qchang/Library/Mobile\ Documents/com~apple~CloudDocs/Dev/ECE-477/Segger\ Internal\ Files/ses_startup_nrf_common.s
