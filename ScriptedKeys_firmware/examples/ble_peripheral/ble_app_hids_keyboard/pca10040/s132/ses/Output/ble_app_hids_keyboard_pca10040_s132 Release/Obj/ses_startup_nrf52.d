e9df583c1ad315a9cb4c20a57b49286207781adc
U-EE07PC16\ee07pc16adm <EE07PC16+ee07pc16adm@EE07PC16.ecn.purdue.edu>
2018-11-17 12:42:32 -0500

    Removed tracking of object files, finally...

--- ScriptedKeys_firmware/examples/ble_peripheral/ble_app_hids_keyboard/pca10040/s132/ses/Output/ble_app_hids_keyboard_pca10040_s132 Release/Obj/ses_startup_nrf52.d
+++
@@ -1,2 +0,0 @@
-Output/ble_app_hids_keyboard_pca10040_s132\ Release/Obj/ses_startup_nrf52.o: \
- /Users/qchang/Library/Mobile\ Documents/com~apple~CloudDocs/Dev/ECE-477/ScriptedKeys_firmware/modules/nrfx/mdk/ses_startup_nrf52.s
--- ScriptedKeys_firmware/examples/ble_peripheral/ble_app_hids_keyboard/pca10040/s132/ses/Output/ble_app_hids_keyboard_pca10040_s132 Release/Obj/utf.d
+++
@@ -1,6 +0,0 @@
-Output/ble_app_hids_keyboard_pca10040_s132\ Release/Obj/utf.o: \
- /Users/qchang/Library/Mobile\ Documents/com~apple~CloudDocs/Dev/ECE-477/ScriptedKeys_firmware/external/utf_converter/utf.c \
- /Users/qchang/Library/Mobile\ Documents/com~apple~CloudDocs/Dev/ECE-477/ScriptedKeys_firmware/external/utf_converter/utf.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.52/include/stddef.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.52/include/__crossworks.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.52/include/stdint.h
